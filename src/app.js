bd7ef1444784c4022284ce54c85ac0b271c45041
Tanuj Mishra <tanujtmmishra@gmail.com>
2025-10-18 00:03:42 +0530

    order of routing in middleware

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('ha ha ha');
+const express = require('express');
+
+const app = express();
+
+app.use('/hello', (req, res) => {
+    res.send('hello');
+})
+
+app.use('/hello/2', (req, res) => {
+    res.send('hey hey hey hello');
+})
+
+
+
+app.use('/test', (req, res) => {
+    res.send('welcome to indian railways');
+})
+
+app.listen(3000, (req, res) => {
+    console.log('my ears are working, thank you for coming at port: 3000');
+})

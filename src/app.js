8bd0e31295acebd4a7a160ac3e27a4cec9f4bff0
Tanuj Mishra <tanujtmmishra@gmail.com>
2025-10-19 02:45:44 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const {database} = require('./config');
+const {User} = require('./models');
 const app = express();
+
+const sampleUser = {
+    firstName: "tanuj",
+    lastName: "mishra",
+    emailId: "tanuj@mishra.com",
+    password: "tanuj@123"
+}
+
+const user = new User(sampleUser);
+
+app.post('/signup', async (req, res) => {
+    await user.save()
+        .then(() =>{
+            res.send('user added successfully');
+        })
+        .catch(() => {
+            res.status(404).send('error occured while saving user');
+        })
+    ;
+
+})
 
 
 database.connectDB()

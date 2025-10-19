6957e8c20d71c5421399b8ea8f7fc9807d61c792
Tanuj Mishra <tanujtmmishra@gmail.com>
2025-10-19 13:30:58 +0530

    reading data from db

--- src/app.js
+++ src/app.js
@@ -3,16 +3,11 @@ const {database} = require('./config');
 const {User} = require('./models');
 const app = express();
 
-const sampleUser = {
-    firstName: "tanuj",
-    lastName: "mishra",
-    emailId: "tanuj@mishra.com",
-    password: "tanuj@123"
-}
 
-const user = new User(sampleUser);
+app.use(express.json());
 
 app.post('/signup', async (req, res) => {
+    const user = new User(req.body);
     await user.save()
         .then(() =>{
             res.send('user added successfully');
@@ -24,6 +19,53 @@ app.post('/signup', async (req, res) => {
 
 })
 
+app.get('/feed', async(req, res) => {
+    try {
+        const result = await User.find({});
+        if(Object.keys(result).length !== 0) {
+            res.send(result);
+        }
+        else {
+            res.status(300).send('no user defined');
+        }
+    } catch (error) {
+        res.status(401).send('internal server error');
+    }
+})
+
+// find user from given id
+// here whenever we provide incorrect id from postman, this directly hits the catch block.
+
+app.get('/users/:_id', async(req, res) => {
+    try {
+        const result = await User.findById(req.params._id).exec();
+        if(!result) {
+            res.status(301).send('no user found for given id');
+        }
+        else {
+            res.send(result);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(401).send('internal server errror');
+    }
+})
+
+
+// find one user by given email id
+app.get('/users', async(req, res) => {
+    try {
+        const result = await User.findOne({emailId: req.body.emailId});
+        if(!result) {
+            res.status(301).send('no user found for given email id');
+        }
+        else {
+            res.send(result);
+        }
+    } catch (error) {
+        res.status(401).send('internal server errror');
+    }
+})
 
 database.connectDB()
     .then(() => {
